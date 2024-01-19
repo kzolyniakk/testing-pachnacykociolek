335a218cfd53993c7abf119e91f0b57098b410ff
Karol Żołyniak <karolzolyniak98@gmail.com>
2024-01-19 16:11:59 +0100

    cypress.config update

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,17 @@ module.exports = {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "https://pachnacykociolek.pl/",
+    redirectionLimit: 3,
+    retries: {
+      "runMode": 1,
+      "openMode": 1
+    },
+    watchForFileChanges: true,
+    chromeWebSecurity: false,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    waitForAnimation: true,
+    testIsolation: false
   },
 };
